13fd9e908b5fb4b63b4c7966985a6d2c6e9c7a3f
Jona535h <jona535h@edu.easj.dk>
2018-09-10 11:19:01 +0200

    Forside og menu færdig. jQuery virker ikke

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready (function () {
+
+//jQuery til burgermenu
+//jQuery til BURGERMENU ÅBEN
+    $('.menuOpen').click(function () {
+        $('.mainMenu').show();
+    });
+//jQuery til BURGERMENU LUKKE
+    $('.menuClose').click(function () {
+        $('.mainMenu').hide();
+    });
+
+
+});
