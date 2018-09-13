16c556e6dbad1905d6f9ab65830c895bce2ea8b0
Jona535h <jona535h@edu.easj.dk>
2018-09-13 16:40:10 +0200

    App færdig

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@
 
 $(document).ready (function () {
 
-//jQuery til burgermenu
-//jQuery til BURGERMENU ÅBEN
+    //jQuery til burgermenu
+    //jQuery til BURGERMENU ÅBEN
     $('.menuOpen').click(function () {
         $('.mainMenu').show();
     });
-//jQuery til BURGERMENU LUKKE
+    //jQuery til BURGERMENU LUKKE
     $('.menuClose').click(function () {
         $('.mainMenu').hide();
     });
 
+    //jQuery til profilside
+    //jQuery til profilside ÅBEN/LUKKE
+    $('.profilIcon').click(function () {
+        $('.profilSide').toggle();
+    });
+
+    //jQuery til tilbage-knap
+    $('.tilbageKnap').click(function(){
+        parent.history.back();
+        return false;
+    });
 
 });
